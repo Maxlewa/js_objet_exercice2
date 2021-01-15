00b424d6633a095db7142dac39256a330d41a213
Maxence <lemairemaxence@hotmail.com>
2021-01-15 12:46:20 +0100

    progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,56 @@
 // EXO 1
 
-// EXO 2
-
-let vieille_dame = {
-    nom() {
-        prenom = "Ludivine"
-        nom = "Dupont"
-    },
-    age : 84,
-    moral : "mal",
-    objet : "canne",
-    sePlaindre() {
-
-    },
-    sePromener() {
-        objet = "perdu"
-    },
+let personne = {
+    argent : 30,
 }
 
-// EXO 3
-
-let vieil_homme = {
-    prenom : "Marcel",
-    age : 90,
-    saluer() {
-        console.log(`Bonjour ${vieille_dame.nom(prenom, nom)}`);
-    },
+let magasin = {
+    chocolat : 2,
+    tartine : 3.5,
+    vendre(a) {
+        let acheter = prompt("Que veux-tu acheter ?")
+        for (const key in this) {
+            console.log(key);
+            if (key == acheter) {
+                console.log(key);
+                console.log(magasin[key]);
+                a.argent == magasin[key]
+            }
+        }
+    }
 }
 
-vieil_homme.saluer(prenom, nom);
-console.log(vieil_homme);
+magasin.vendre(personne);
+console.log(magasin);
+console.log(personne);
+
+// // EXO 2
+
+// let vieille_dame = {
+//     nom : {
+//         prenom : "Ludivine",
+//         nom : "Dupont",
+//     },
+//     age : 84,
+//     moral : "mal",
+//     objet : "canne",
+//     sePlaindre() {
+
+//     },
+//     sePromener() {
+//         objet = "perdu"
+//     },
+// }
+
+// // EXO 3
+
+// let vieil_homme = {
+//     prenom : "Marcel",
+//     age : 90,
+//     saluer() {
+//         console.log(`Bonjour ${vieille_dame.nom.prenom} ${vieille_dame.nom.nom}`);
+//     },
+// }
+
+// vieil_homme.saluer();
+// console.log(vieil_homme);
